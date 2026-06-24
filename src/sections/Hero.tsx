dc77e44991333db5781787057355a26da928493b
piin_nhs <skygame2709@gmail.com>
2026-06-24 17:10:38 +0700

    style: lower quote position and add margin to author block

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -198,13 +198,13 @@ export const Hero: React.FC = () => {
                   <span>Code is like humor. When you have to explain it, it's bad.</span>
                   <span 
                     className="absolute text-4xl bg-background px-1 text-accent font-bold select-none"
-                    style={{ right: '1.5rem', bottom: '-0.6rem' }}
+                    style={{ right: '1.5rem', bottom: '-1.0rem' }}
                   >
                     ”
                   </span>
                 </p>
               </div>
-              <p className="w-fit border border-[#3e4452] border-t-transparent py-1.5 px-3 text-textSecondary text-xs font-mono">
+              <p className="w-fit border border-[#3e4452] border-t-transparent py-1.5 px-3 text-textSecondary text-xs font-mono mr-10">
                 - Cory House
               </p>
             </div>
